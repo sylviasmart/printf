bef805213a55cf895418d96eb8df08f83a614762
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-07-24 19:09:43 +0000

    task 0

--- printf.c
+++ printf.c
@@ -5,17 +5,17 @@
 #include <unistd.h>
 
 /**
-* _printf - A function used to print a string to stdout
-* @format: A character string
-*
-* Return: printf string
+ * _printf - A function used to print a formatted string to stdout
+ * @format: A character string containing the format and data to print
+ *
+ * Return: The total number of characters printed (excluding the null byte)
 */
 
 int _printf(const char *format, ...)
 {
 va_list args;
+int available_characters_to_count = 0;
 va_start(args, format);
-int avialable_characters_to_count = 0;
 
 while (*format)
 {
@@ -25,12 +25,18 @@ format++;
 if (*format == 'c')
 available_characters_to_count += _putchar(va_arg(args, int));
 else if (*format == 's')
-for (const char *s = va_arg(args, const char *); *s; s++)
+{
+const char *s = va_arg(args, const char *);
+while (*s)
+{
 available_characters_to_count += _putchar(*s);
+s++;
+}
+}
 else if (*format == '%')
 available_characters_to_count += _putchar('%');
 else
-avialable_characters_to_count += _putchar('%') + _putchar(*format);
+available_characters_to_count += _putchar('%') + _putchar(*format);
 }
 else
 {
@@ -43,4 +49,3 @@ format++;
 va_end(args);
 return (available_characters_to_count);
 }
-
