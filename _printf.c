3d1ea22d57009b5b141db57c546f4c36e4b30b92
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-07-24 20:57:24 +0000

    task 0

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _printf - A function used to print a formatted string to stdout
+ * @format: A character string containing the format and data to print
+ *
+ * Return: The total number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int available_characters_to_count = 0;
+va_start(args, format);
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+available_characters_to_count += _putchar(va_arg(args, int));
+else if (*format == 's')
+{
+const char *s = va_arg(args, const char *);
+while (*s)
+{
+available_characters_to_count += _putchar(*s);
+s++;
+}
+}
+else if (*format == '%')
+available_characters_to_count += _putchar('%');
+else
+available_characters_to_count += _putchar('%') + _putchar(*format);
+}
+else
+{
+available_characters_to_count += _putchar(*format);
+}
+
+format++;
+}
+
+va_end(args);
+return (available_characters_to_count);
+}
+
